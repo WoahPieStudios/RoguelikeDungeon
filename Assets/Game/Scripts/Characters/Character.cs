a728ef30ef63efc625f06a9d9bdf598afade4a6e
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2021-10-16 17:48:30 +0800

    Test - Added empty Character Class

---
+++ Assets/Game/Scripts/Characters/Character.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Game.Characters
+{
+    public class Character : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
