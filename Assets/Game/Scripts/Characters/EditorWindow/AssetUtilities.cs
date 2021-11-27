c71b97a41cf1010527cf85c9ce99672ca765e160
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2021-11-27 15:24:17 +0800

    saving changes from merge

--- Assets/Game/Scripts/Characters/EditorWindow/AssetUtilities.cs
+++ Assets/Game/Scripts/Characters/EditorWindow/AssetUtilities.cs
@@ -14,13 +14,6 @@ namespace Game.CharactersEditor
 {
     public static class AssetUtilities 
     {
-<<<<<<< HEAD:Assets/Game/Scripts/Characters/EditorWindow/AssetUtilities.cs
-=======
-        static readonly Type[] _CreatableAssetRootTypes = new Type[] { typeof(CharacterData), typeof(Characters.Action) };
-        
-        public static Type[] creatableAssetRootTypes => _CreatableAssetRootTypes;
-
->>>>>>> ee4d850124339e48857a7472963abbf7e8acd4ca:Assets/Game/Scripts/Characters/EditorWindow/Utilities.cs
         public static void CreateFolder(string path)
         {
             string currentPath = "";
