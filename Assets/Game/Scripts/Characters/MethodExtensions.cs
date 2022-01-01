2154acffd497413f47381d6e176838d10c42966f
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-01-01 18:53:07 +0800

    Added Another FaceNearestCharacter function, directly orients character towards given target

--- Assets/Game/Scripts/Characters/MethodExtensions.cs
+++ Assets/Game/Scripts/Characters/MethodExtensions.cs
@@ -21,13 +21,27 @@ namespace Game.Characters
             T nearestCharacter = Utilities.GetNearestCharacter<T>(characterBase.transform.position, radius, characterBase);
 
             if(nearestCharacter)
-            {
-                Vector2 direction = nearestCharacter.transform.position - characterBase.transform.position;
-
-                characterBase.Orient(Vector2Int.RoundToInt(direction.normalized));
-            }
+                FaceNearestCharacter(characterBase, nearestCharacter);
 
             return nearestCharacter;
+        }
+
+        /// <summary>
+        /// Orients the Character towards the nearest Character.
+        /// </summary>
+        /// <param name="characterBase">Character to be ignored when finding the nearest character</param>
+        /// <param name="radius">Radius around the character</param>
+        /// <param name="characterLayer">Layer Mask for where the Characters are</param>
+        /// <typeparam name="T">Generic For what class the Character is derived from. (Hero, Enemy, or CharacterBase)</typeparam>
+        /// <returns>Returns the Nearest T Character</returns>
+        public static void FaceNearestCharacter(this CharacterBase characterBase, CharacterBase target)
+        {
+            if(!target)
+                return;
+
+            Vector2 direction = target.transform.position - characterBase.transform.position;
+
+            characterBase.Orient(Vector2Int.RoundToInt(direction.normalized));
         }
 
         /// <summary>
@@ -41,7 +55,11 @@ namespace Game.Characters
         {
             return FaceNearestCharacter<CharacterBase>(characterBase, radius);
         }
-
+        
+        /// <summary>
+        /// Returns casted variable if it inherits from TCast. If not, null. 
+        /// </summary>
+        /// <typeparam name="TCast"></typeparam>
         public static TCast IsCast<TCast>(this object source) where TCast : class
         {
             return source is TCast ? source as TCast : null;
