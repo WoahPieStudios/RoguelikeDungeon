c21c301f89fe4c3c430e69a5048891f79efaab74
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2022-02-12 15:01:02 +0800

    saving progress

--- Assets/Game/Scripts/Characters/Animations/AnimationController.cs
+++ Assets/Game/Scripts/Characters/Animations/AnimationController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -12,15 +13,17 @@ namespace Game.Characters.Animations
 
         Animation _Animation;
 
-        Dictionary<string, AnimationClipAndEvents> _AnimationClipAndEventsDictionary = new Dictionary<string, AnimationClipAndEvents>();
+        Dictionary<string, AnimationData> _AnimationDatas = new Dictionary<string, AnimationData>();
 
-        struct AnimationClipAndEvents
+        struct AnimationData
         {
+            public bool isPlaying;
             public AnimationClip animationClip;
             public System.Action[] animationEvents;
 
-            public AnimationClipAndEvents(AnimationClip animationClip, System.Action[] animationEvents)
+            public AnimationData(AnimationClip animationClip, System.Action[] animationEvents)
             {
+                this.isPlaying = false;
                 this.animationClip = animationClip;
                 this.animationEvents = animationEvents;
             }
@@ -30,34 +33,46 @@ namespace Game.Characters.Animations
         {
             _Animation = GetComponent<Animation>();
         }
-
+        
         public void AddAnimation(string name, AnimationClip animationClip, params System.Action[] animationEvents)
         {
-            _AnimationClipAndEventsDictionary.Add(name, new AnimationClipAndEvents(animationClip, animationEvents));
+            _AnimationDatas.Add(name, new AnimationData(animationClip, animationEvents));
 
             _Animation.AddClip(animationClip, name);
         }
 
         public void RemoveAnimation(string name)
         {
-            if(!_AnimationClipAndEventsDictionary.ContainsKey(name))
+            if(!Contains(name))
                 return;
 
-            AnimationClip animationClip = _AnimationClipAndEventsDictionary[name].animationClip;
+            AnimationClip animationClip = _AnimationDatas[name].animationClip;
 
             _Animation.RemoveClip(animationClip);
 
-            _AnimationClipAndEventsDictionary.Remove(name);
+            _AnimationDatas.Remove(name);
+        }
+
+        public string[] GetPlayingAnimation()
+        {
+            return _AnimationDatas.Where(p => p.Value.isPlaying).Select(p => p.Key).ToArray();
         }
 
         public void Play(string name)
         {
-            if(!_AnimationClipAndEventsDictionary.ContainsKey(name))
+            if(!Contains(name))
                 return;
 
-            _CurrentAnimationEvents = _AnimationClipAndEventsDictionary[name].animationEvents;
+            AnimationData animationData = _AnimationDatas[name];
 
+            animationData.isPlaying = false;
+            _CurrentAnimationEvents = animationData.animationEvents;
             _Animation.Play(name);
+        }
+
+        public bool Contains(string name)
+        {
+            return _AnimationDatas.ContainsKey(name);
         }
 
         public void Stop()
@@ -67,10 +82,16 @@ namespace Game.Characters.Animations
 
         public void Stop(string name)
         {
-            if(!_AnimationClipAndEventsDictionary.ContainsKey(name))
+            if(!Contains(name))
                 return;
+            
+            AnimationData animationData = _AnimationDatas[name];
 
-            _Animation.Stop(name);
+            if(animationData.isPlaying)
+            {
+                _Animation.Stop(name);
+                animationData.isPlaying = false;
+            }
         }
 
         public void CrossFadePlay(string name, float fadeTime)
